04c252558bec8a3d693400541257b30c6f905882
Liamhewitt05 <112162777+Liamhewitt05@users.noreply.github.com>
2023-03-10 20:57:52 +0100

    Create Refleksjon.m

---
+++ Refleksjon.m
@@ -0,0 +1,11 @@
+Refleksjon:
+Synes det var en gøy og utfordrene oppgave og fikk lært mye nytt. Møtte på mange problemer men var gøy og motiverende og finne løsninger og se programme utvikle seg.
+
+Dette er jeg fornøyd med:
+Jeg er fornøyd med innsatsen min, og hvordan sluttresultatet ble. De aller fleste delen av programmet fungerte som planlagt som var bra og se også. 
+
+Dette er jeg ikke like fornøyd med:
+Sliter av og til med å finne ut problemer selv, og ender opp med å spør om hjelp. Men ved å spørre om hjlep lærer jeg også noe nytt.
+
+Slik kan det bli bedre neste gang:
+Programmet var litt rotette noen steder. Til neste gang skal jeg prøve å dele opp programmet i mer funskjoner som gjør det mer oversktelig.
